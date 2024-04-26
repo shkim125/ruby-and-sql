f88059caa60e5c737d187fb9fcf3fcd051c24429
Sarah Kim <sarah.kim@kellogg.northwestern.edu>
2024-04-26 20:29:25 +0000

    code along 2

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -14,14 +14,60 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+
 # 2. insert new rows in companies table
+new_company = Company.new
+# p new_company
+new_company["name"] = "Apple"
+new_company["city"] = "Cupertino"
+new_company["state"] = "CA"
+new_company["url"] = "https://apple.com"
+# p new_company
+new_company.save
+# p new_company
+
+company = Company.new
+company["name"] = "Amazon"
+company["city"] = "Seattle"
+company["state"] = "WA"
+company.save
+
+company = Company.new
+company["name"] = "Twitter"
+company["city"] = "San Francisco"
+company["state"] = "CA"
+company.save
+
+puts "There are #{Company.all.count} companies"
 
 # 3. query companies table to find all row with California company
+all_companies = Company.all
+# p all_companies
+cali_companies = Company.where({"state" => "CA"})
+# p cali_companies
+
 
 # 4. query companies table to find single row for Apple
 
+apple = Company.find_by({"name" => "Apple"})
+# p apple
+
 # 5. read a row's column value
+p apple["name"]
 
 # 6. update a row's column value
+twitter = Company.find_by({"name" => "Twitter"})
+# p twitter
+twitter["url"] = "https://twitter.com"
+# p twitter
+twitter["name"] = "X (Formerly Twitter)"
+# p twitter
+twitter.save
 
 # 7. delete a row
+x = Company.find_by({"name" => "X (Formerly Twitter)"})
+x.destroy
+
+
+puts "There are #{Company.all.count} companies"
+puts "California companies: #{cali_companies.count}"
